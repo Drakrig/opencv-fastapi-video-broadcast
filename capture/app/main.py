301114cc97ea4fda235edeb1c9b63fab7d4e8dea
drakrig <drakrig@gmail.com>
2024-01-01 14:10:42 +0300

    Major changes in capture/app/main.py
    * Changed description in line argument help for destination
    * Remove unused url (now using destination variable directly insted)
    * Add port cast to int
    * Decrease waiting for server from 10 to 5
    * Fix socket connection
    * Change video_process(). Now have 2 args - source and socket.

--- capture/app/main.py
+++ capture/app/main.py
@@ -10,7 +10,7 @@ import cv2
 # Networking
 import socket
 
-async def process_video(source):
+def process_video(source, client_socket):
     print("Conect to source")
     cap = cv2.VideoCapture(source)
     ret, frame = cap.read()
@@ -38,7 +38,7 @@ parser = ArgumentParser(description="Entry point for video sender. Press 's' to 
 
 parser.add_argument("-src","--source", default="", help="Source for video")
 parser.add_argument("-sf", "--skip_frames", type=int, default=0, help="How many frames skip in process to increase speed")
-parser.add_argument("-dst","--destination", default="", help="Destination IP address with port")
+parser.add_argument("-dst","--destination", default="", help="Destination IP address with port like address:port")
 
 
 args = vars(parser.parse_args())
@@ -46,22 +46,20 @@ args = vars(parser.parse_args())
 source = args["source"]
 skip_frames=args["skip_frames"]
 destination, port = args["destination"].split(":")
-
-# Initialization 10.10.10.1:80/video
-url = f'http://{destination}'
+port = int(port) 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("Connect to server")
-sleep(10)
+sleep(5)
 while True:
     try:
-        client_socket.connect((url, port))
+        client_socket.connect((destination, port))
         break
     except:
         print("Server not awaliable")
         sleep(1)
 print("Start main working process")
 #asyncio.run(process_video(source))
-process_video(source)
+process_video(source, client_socket)
 
